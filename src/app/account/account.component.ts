fa2aa301159d886d6f8cbcacdb87db94329ff96e
omar1z <omarzammar11@gmail.com>
2024-07-15 10:01:17 +0300

    creating a data service so we added the account service that have the data and functions and we injected this service inside of our components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-account',
@@ -10,12 +11,13 @@ import { LoggingService } from '../logging.service';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService,
+    private accountService: AccountService
+  ) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountService.updateAccount(this.id, status)
     // console.log('A server status changed, new status: ' + status);
     this.loggingService.logStatusChange(status)
   }
