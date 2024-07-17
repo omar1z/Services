ac828632e307656bc5a444dd65ba02fb06b50322
omar1z <omarzammar11@gmail.com>
2024-07-17 11:06:39 +0300

    forms using template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  @ViewChild('f') signUpForm: NgForm;
+  user = { 
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({ // set all form
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'teacher',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signUpForm.form.patchValue({ // override parts of the form
+      userData: {
+        username: suggestedName
+      }
+    })
+  }
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.signUpForm.reset();
   }
 }
